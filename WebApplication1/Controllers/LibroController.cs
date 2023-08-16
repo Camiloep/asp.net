fd49ee6a8cca02e1da1e6f60e61e00e01806231b
Camilo Estrada - Sena <cestrada080@misena.edu.co>
2023-08-16 17:22:22 -0500

    Segundo Commit

--- WebApplication1/Controllers/LibroController.cs
+++ WebApplication1/Controllers/LibroController.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class LibroController : Controller
     {
+        // Esto es para acceder a la base de datos
+        private readonly ApplicationDbContext _context;
+
+        // Constructor
+        public LibroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Metodo Get index
         public IActionResult Index()
         {
+            // Creamos lista de datos IEnumerable
+            IEnumerable<Libros> ListLibros = _context.Libros;
+            return View(ListLibros); // Retorne la lista
+        }
+        // HTTP Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST create
+        [HttpPost]
+
+        public IActionResult Create(Libros libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libros.Add(libro);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
 }
+
--- WebApplication1/Models/Libros.cs
+++ WebApplication1/Models/Libros.cs
@@ -1,15 +1,23 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Libros
     {
+        [Required(ErrorMessage = "Campo Obligatorio")]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Campo Obligatorio")]
         public string Titulo { get; set; }
 
+        [Required(ErrorMessage = "Campo Obligatorio")]
         public string Descripcion { get; set; }
 
+        [Required(ErrorMessage = "Campo Obligatorio")]
         public string libros { get; set; }
 
+        [Required(ErrorMessage = "Campo Obligatorio")]
         public double precio { get; set; }
+
     }
 }
